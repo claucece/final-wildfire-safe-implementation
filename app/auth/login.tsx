da212db777250c37c7a10e8ab8c1f4db668a02d6
Sofía Celi <cherenkovd69@gmail.com>
2026-03-19 17:23:42 +0000

    Refactor login

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -8,7 +8,7 @@ import {
   KeyboardAvoidingView,
   ActivityIndicator,
   Platform,
-  ScrollView
+  ScrollView,
 } from "react-native";
 
 import { useRouter } from "expo-router";
@@ -34,6 +34,10 @@ import logInImage from "@/assets/app-images/log-in-image.webp";
 
 export default function Login() {
   const orientation = useOrientation();
+  // Determine if portrait
+  const isPortrait = orientation === "PORTRAIT";
+
+  // Form to login
   const { formData, handleInputChange } = useForm();
 
   const [error, setError] = useState<string | null>(null);
@@ -58,11 +62,14 @@ export default function Login() {
     if (!validateForm()) return;
 
     setLoading(true); // Show loading indicator
-    setError(null);   // Clear any previous error
+    setError(null); // Clear any previous error
 
     try {
       const { email, password } = formData;
-      const { success, username, errorMessage } = await loginUser(email, password);
+      const { success, username, errorMessage } = await loginUser(
+        email,
+        password,
+      );
 
       if (success) {
         setLoading(false);
@@ -87,79 +94,119 @@ export default function Login() {
       <Image
         source={logInImage}
         contentFit="cover"
-        placeholder={{ blurhash: BLUR_HASH_DATA[2]?.hash || "L39[3oI8tuN84?tMIK?Z*F.O4V4Y" }}
+        placeholder={{
+          blurhash: BLUR_HASH_DATA[2]?.hash || "L2FF24Ed?QMU0CvC=:XT2|CQ$+WG",
+        }}
         accessibilityLabel="Cozy background image depicting a serene retro environment for login"
         accessible
         accessibilityRole="image"
         style={[
           styles.imageContainer,
-          orientation === 'PORTRAIT' ? styles.portraitImage : styles.landscapeImage
+          orientation === "PORTRAIT"
+            ? styles.portraitImage
+            : styles.landscapeImage,
         ]}
       />
       <CustomGradient colors={[Colors.gradientMid, Colors.gradientDarkLight]}>
-        <SafeAreaView style={[styles.safeAreaContainer, styles.safeAreaContainerAuth]}>
+        <SafeAreaView
+          style={[styles.safeAreaContainer, styles.safeAreaContainerAuth]}
+        >
           {/* Back Button */}
-          <BackButton orientation={orientation} size={orientation === 'PORTRAIT' ? 40 : 20} />
+          <BackButton
+            orientation={orientation}
+            customStyle={isPortrait ? styles.buttonNorm : styles.buttonLand}
+            size={isPortrait ? 40 : 50}
+          />
 
           {/* Title container */}
           <View
             style={[
               styles.pixelPanel,
-              orientation === 'PORTRAIT' ? styles.textContainerAuth : styles.textContainerAuthLandscape
+              isPortrait
+                ? styles.textContainerAuth
+                : styles.textContainerAuthLandscape,
             ]}
           >
-            <Text style={[styles.mainTitle, styles.pixelTitle]}>Log In</Text>
+            <Text
+              style={[
+                styles.mainTitle,
+                styles.pixelTitle,
+                isPortrait ? styles.buttonNorm : styles.pixelTitleLand,
+              ]}
+            >
+              Log In
+            </Text>
             <Text style={[styles.pixelSubtleText]}>
               Continue your preparedness progress
             </Text>
           </View>
 
-          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-          {/* Ensure the login form remains scrollable even when the keyboard pops */}
-          <ScrollView
-            contentContainerStyle={styles.formScroll}
-            keyboardShouldPersistTaps="handled"
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
           >
-          {/* Form Inputs */}
-            <View style={[styles.pixelPanel, styles.formPanel]}>
-            {["email", "password"].map((field, index) => (
-              <TextInput
-                key={index}
-                style={[
-                  orientation === 'PORTRAIT' ? styles.input : styles.inputLandscape
-                ]}
-                placeholder={`Your ${field.charAt(0).toUpperCase() + field.slice(1)}`}
-                placeholderTextColor="#EAEAEA"
-                value={formData[field as keyof typeof formData]}
-                onChangeText={(value) => handleInputChange(field as keyof typeof formData, value)}
-                secureTextEntry={field.toLowerCase().includes("password")}
-                autoCapitalize={"none"}
-                keyboardType={field === "email" ? "email-address" : "default"}
-                textContentType={["password"].includes(field) ? "oneTimeCode" : "none"}
-                accessibilityLabel={field}
-              />
-            ))}
+            {/* Ensure the login form remains scrollable even when the keyboard pops */}
+            <ScrollView
+              contentContainerStyle={styles.formScroll}
+              keyboardShouldPersistTaps="handled"
+            >
+              {/* Form Inputs */}
+              <View style={[styles.pixelPanel, styles.formPanel]}>
+                {["email", "password"].map((field, index) => (
+                  <TextInput
+                    key={index}
+                    style={[
+                      orientation === "PORTRAIT"
+                        ? styles.input
+                        : styles.inputLandscape,
+                    ]}
+                    placeholder={`Your ${field.charAt(0).toUpperCase() + field.slice(1)}`}
+                    placeholderTextColor="#EAEAEA"
+                    value={formData[field as keyof typeof formData]}
+                    onChangeText={(value) =>
+                      handleInputChange(field as keyof typeof formData, value)
+                    }
+                    secureTextEntry={field.toLowerCase().includes("password")}
+                    autoCapitalize={"none"}
+                    keyboardType={
+                      field === "email" ? "email-address" : "default"
+                    }
+                    textContentType={
+                      ["password"].includes(field) ? "oneTimeCode" : "none"
+                    }
+                    accessibilityLabel={field}
+                  />
+                ))}
 
-            {/* Display error message if any */}
-            {error && <Text style={styles.errorText} accessibilityLabel="error fields">{error}</Text>}
+                {/* Display error message if any */}
+                {error && (
+                  <Text
+                    style={styles.errorText}
+                    accessibilityLabel="error fields"
+                  >
+                    {error}
+                  </Text>
+                )}
 
-            {/* Show either the loading indicator or the login button */}
-            {loading ? (
-              <ActivityIndicator size="small" style={styles.activityIndicator} />
-            ) : (
-              <CustomAnimatedButton
-                onPress={loading ? null : handleLogin}
-                title="Log In"
-                textStyles={styles.buttonText}
-                accessibilityLabel="Login button"
-              />
-            )}
-          </View>
-          </ScrollView>
+                {/* Show either the loading indicator or the login button */}
+                {loading ? (
+                  <ActivityIndicator
+                    size="small"
+                    style={styles.activityIndicator}
+                  />
+                ) : (
+                  <CustomAnimatedButton
+                    onPress={loading ? null : handleLogin}
+                    title="Log In"
+                    textStyles={styles.buttonText}
+                    accessibilityLabel="Login button"
+                  />
+                )}
+              </View>
+            </ScrollView>
           </KeyboardAvoidingView>
 
           {/* Light-themed status bar */}
-          <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+          <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
         </SafeAreaView>
       </CustomGradient>
     </View>
